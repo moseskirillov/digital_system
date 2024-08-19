0065bd2770d6e61c370a9c06cf5cb595b851bfbc
Moses Kirillov <moisey.kirillov@gmail.com>
2024-08-19 21:48:22 +0300

    Обновил форматирование запросов в ReportRepository

--- src/main/java/org/wolrus/digital_system/repository/ReportRepository.java
+++ src/main/java/org/wolrus/digital_system/repository/ReportRepository.java
@@ -8,25 +8,30 @@ import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
-    @Query(value = "select count(*) " +
-            "from homegroup_bot.reports r " +
-            "where r.group_is_done = true " +
-            "and date >= (current_date - interval '1 month')", nativeQuery = true)
+    @Query(value = """
+            select count(*)
+            from homegroup_bot.reports r
+            where r.group_is_done = true
+            and date >= (current_date - interval '1 month')
+            """, nativeQuery = true)
     Integer countAllCompletedGroupsByMonth();
 
-    @Query(value = "select count(*) " +
-            "from homegroup_bot.reports r " +
-            "where r.group_is_done = false " +
-            "and r.date >= (current_date - interval '1 month')", nativeQuery = true)
+    @Query(value = """
+            select count(*)
+            from homegroup_bot.reports r
+            where r.group_is_done = false
+            and r.date >= (current_date - interval '1 month')
+            """, nativeQuery = true)
     Integer countAllNotCompletedGroupsByMonth();
 
-    @Query(value = "select r.n, r.c " +
-            "from (select leader_name n, count(leader_name) as c " +
-            "from homegroup_bot.reports " +
-            "where group_is_done = false " +
-            "and date >= (current_date - interval '1 month') " +
-            "group by leader_name) r " +
-            "where c > 3", nativeQuery = true)
+    @Query(value = """
+            select r.n, r.c
+            from (select leader_name n, count(leader_name) as c
+            from homegroup_bot.reports
+            where group_is_done = false
+            and date >= (current_date - interval '1 month')
+            group by leader_name) r
+            where c > 3""", nativeQuery = true)
     List<Object[]> findThreeTimesNotCompletedGroupsByMonth();
 
 }
